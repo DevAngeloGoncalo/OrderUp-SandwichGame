dedb4b21062aabfdf3d45d473870c975be787850
Ângelo Gonçalo <45950068+DevAngeloGoncalo@users.noreply.github.com>
2023-06-14 23:47:09 -0300

    OrderController

---
+++ Assets/Scripts/OrderController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OrderController : MonoBehaviour
+{
+    public SandwichObject sandwich;
+    public Button hamburgerButton;
+    public Button cheeseButton;
+    public Button lettuceButton;
+    public Button finishOrderButton;
+
+    List<string> selectedIngredients = new List<string>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hamburgerButton.onClick.AddListener(AddHamburger);
+        cheeseButton.onClick.AddListener(AddCheese);
+        lettuceButton.onClick.AddListener(AddLettuce);
+        finishOrderButton.onClick.AddListener(FinishOrder);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void AddHamburger()
+    {
+        selectedIngredients.Add("Hamburger");
+    }
+
+    void AddCheese()
+    {
+        selectedIngredients.Add("Cheese");
+    }
+
+    void AddLettuce()
+    {
+        selectedIngredients.Add("Lettuce");
+    }
+
+    void FinishOrder()
+    {
+        //bool correctIngredients = (hasHamburger && hasCheese && hasLettuce);
+        bool correctOrder = CheckOrder();
+
+        //if (correctIngredients && correctOrder)
+        if (correctOrder)
+        {
+            Debug.Log("Order completed successfully!");
+            // Execute any desired actions upon successful order completion
+        }
+        else
+        {
+            Debug.Log("Order is incorrect. Please check the ingredients and order.");
+            // Execute any desired actions upon incorrect order
+        }
+    }
+
+    bool CheckOrder()
+    {
+
+        if (selectedIngredients.Count != sandwich.ingredients.Length)
+            return false;
+
+        for (int i = 0; i < selectedIngredients.Count; i++)
+        {
+            if (selectedIngredients[i] != sandwich.ingredients[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
